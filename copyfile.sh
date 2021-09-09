da77ecbf5b4a21963a8c61bcd4cd52619efa9b4b
gongpingzz <72126751+gongpingzz@users.noreply.github.com>
2021-09-09 16:57:17 +0800

    Update copyfile.sh

--- copyfile.sh
+++ copyfile.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+file="1.txt"
+
+while IFS= read -r line
+do
+        # display $line or do somthing with $line
+        echo ${line}
+        filepath=$(find -name ${line})
+        echo ${filepath}
+        mv ${filepath} ./dstdir/
+done <"$file"
